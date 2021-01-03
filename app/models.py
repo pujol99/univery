4fa56c7ef53ace3b77d956148364bd973a8948f0
Alex Pujol Comet <alexpujolcomet8@gmail.com>
2021-01-03 19:10:29 +0100

    new db model

--- app/models.py
+++ app/models.py
@@ -1,6 +1,18 @@
-from app import db, login_manager
+from app import login_manager, db
 from flask_login import UserMixin
 
+deliveries = db.Table('deliveries',
+    db.Column('delivery_id', db.Integer, db.ForeignKey('delivery.id'), primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('isDone', db.Boolean, nullable=False, default=False),
+    db.Column('isEliminated', db.Boolean, nullable=False, default=False)
+)
+
+subjects = db.Table('subjects',
+    db.Column('subject_id', db.Integer, db.ForeignKey('subject.id'), primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('color', db.String(10), nullable=False, default="#000")
+)   
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -11,23 +23,16 @@ class User(db.Model, UserMixin):
     fullname = db.Column(db.String(50), nullable=False)
     identification = db.Column(db.String(15), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    deliveries = db.relationship('Delivery', backref='user', lazy=True)
-    subjects = db.relationship('Subject', backref='user', lazy=True)
     last_update = db.Column(db.DateTime, nullable=True)
+
+    deliveries = db.relationship('Delivery', secondary=deliveries, lazy='subquery',
+        backref=db.backref('users', lazy=True))
+    subjects = db.relationship('Subject', secondary=subjects, lazy='subquery',
+        backref=db.backref('users', lazy=True))
 
     def __repr__(self):
         return f"User('{self.fullname}', '{self.identification }')"
 
-class Subject(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    identification = db.Column(db.String(15), nullable=False)
-    name = db.Column(db.String(50), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    deliveries = db.relationship('Delivery', backref='subject', lazy=True)
-    color = db.Column(db.String(10), nullable=False, default="#ffffff")
-
-    def __repr__(self):
-        return f"Subject('{self.name}', '{self.identification }')"
 
 class Delivery(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -36,10 +41,16 @@ class Delivery(db.Model):
     toDate = db.Column(db.DateTime, nullable=True)
     toDateStr = db.Column(db.String(20), nullable=True)
     url = db.Column(db.String(300), nullable=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
-    isDone = db.Column(db.Boolean, nullable=False, default=False)
-    isEliminated = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__(self):
-        return f"Delivery('{self.name}', '{self.toDateStr }')"
+        return f"Delivery('{self.name}', '{self.toDateStr}')"
+
+
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    identification = db.Column(db.String(15), nullable=False)
+    name = db.Column(db.String(50), nullable=False)
+
+    def __repr__(self):
+        return f"Subject('{self.name}', '{self.identification }')"
